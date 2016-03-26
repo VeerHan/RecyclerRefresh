cb38d671722ae09063d36cda1bd44e9461dad1be
Leo.Han <leoleohan@foxmail.com>
2016-03-26 15:25:29 +0800

    update

--- app/src/main/java/com/leohan/refresh/MainActivity.java
+++ app/src/main/java/com/leohan/refresh/MainActivity.java
@@ -1,13 +1,165 @@
 package com.leohan.refresh;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * 重要公告
+ *
+ * @author Leo
+ */
 public class MainActivity extends AppCompatActivity {
 
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.tv_tile)
+    TextView tvTile;
+    @InjectView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @InjectView(R.id.SwipeRefreshLayout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    boolean isLoading;
+    private List<Map<String, Object>> data = new ArrayList<>();
+    private NoticeAdapter adapter = new NoticeAdapter(this, data);
+    private Handler handler = new Handler();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notice);
+        ButterKnife.inject(this);
+        initView();
+        initData();
     }
+
+    public void initView() {
+        tvTile.setText(R.string.notice);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        swipeRefreshLayout.setColorSchemeResources(R.color.blueStatus);
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        data.clear();
+                        getData();
+                    }
+                }, 3000);
+            }
+        });
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.d("test", "StateChanged = " + newState);
+
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Log.d("test", "onScrolled");
+
+                int lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
+                if (lastVisibleItemPosition + 1 == adapter.getItemCount()) {
+                    Log.d("test", "loading executed");
+
+                    boolean isRefreshing = swipeRefreshLayout.isRefreshing();
+                    if (isRefreshing) {
+                        adapter.notifyItemRemoved(adapter.getItemCount());
+                        return;
+                    }
+                    if (!isLoading) {
+                        isLoading = true;
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                getData();
+                                Toast.makeText(MainActivity.this, "load more completed", Toast.LENGTH_SHORT).show();
+                                isLoading = false;
+                            }
+                        }, 1000);
+                    }
+                }
+            }
+        });
+
+        //添加点击事件
+        adapter.setOnItemClickLitener(new NoticeAdapter.OnItemClickLitener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Log.d("test", "item position = " + position);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        });
+    }
+
+
+    public void initData() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getData();
+            }
+        }, 100);
+
+    }
+
+    /**
+     * 获取类别数据
+     */
+    private void getData() {
+        for (int i = 0; i < 6; i++) {
+            Map<String, Object> map = new HashMap<>();
+            data.add(map);
+        }
+        adapter.notifyDataSetChanged();
+        swipeRefreshLayout.setRefreshing(false);
+        adapter.notifyItemRemoved(adapter.getItemCount());
+    }
+
+
 }
---
+++ app/src/main/java/com/leohan/refresh/NoticeAdapter.java
@@ -0,0 +1,109 @@
+package com.leohan.refresh;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NoticeAdapter extends Adapter<ViewHolder> {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_FOOTER = 1;
+    private Context context;
+    private List data;
+
+    public interface OnItemClickLitener {
+        void onItemClick(View view, int position);
+
+        void onItemLongClick(View view, int position);
+    }
+
+    private OnItemClickLitener onItemClickLitener;
+
+    public void setOnItemClickLitener(OnItemClickLitener onItemClickLitener) {
+        this.onItemClickLitener = onItemClickLitener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size() == 0 ? 0 : data.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position + 1 == getItemCount()) {
+            return TYPE_FOOTER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    public NoticeAdapter(Context context, List data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_notice, parent,
+                    false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_foot, parent,
+                    false);
+            return new FootViewHolder(view);
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            //holder.tv.setText(data.get(position));
+            if (onItemClickLitener != null) {
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int pos = holder.getLayoutPosition();
+                        onItemClickLitener.onItemClick(holder.itemView, pos);
+                    }
+                });
+
+                holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        int pos = holder.getLayoutPosition();
+                        onItemClickLitener.onItemLongClick(holder.itemView, pos);
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+
+
+    static class ItemViewHolder extends ViewHolder {
+
+        TextView tv;
+
+        public ItemViewHolder(View view) {
+            super(view);
+            tv = (TextView) view.findViewById(R.id.tv_date);
+        }
+    }
+
+    static class FootViewHolder extends ViewHolder {
+
+        public FootViewHolder(View view) {
+            super(view);
+        }
+    }
+}
